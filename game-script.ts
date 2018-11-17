36bb0bf0433552e21eb427cbdad2b7021823cc3c
Lindsay Zhou <43657142+lin-zhou@users.noreply.github.com>
2018-11-17 05:27:58 -0500

    Update game-script.ts
    Jump ability for Cyrus

--- game-script.ts
+++ game-script.ts
@@ -1,17 +1,17 @@
 import {
     Sprite,
-    Container,
     Application,
-    Rectangle,
-    Graphics,
-    DisplayObject,
-    Text
 } from "pixi.js";
+
+// NOTE: Write "on ground" test
 
 // SET UP
 
 const app: Application = new Application(1024 * .85, 576 * .85);
 document.body.appendChild(app.view);
+
+let acc: number = 0.05;
+let cyrusV: number = 0;
 
 class Enemy {
     sprite: Sprite;
@@ -78,6 +78,7 @@ window.addEventListener("keydown", (e: KeyboardEvent): void  => {
         }
     } else if (e.keyCode === UP) {
         W = -1;
+        cyrusV = -4;
     } else if (e.keyCode === RIGHT) {
         D = 1;
         if (cyrus.scale.x >= 0 ) {
@@ -156,14 +157,6 @@ window.addEventListener("keyup", (e: KeyboardEvent): void  => {
     }
 },                      false);
 
-// NOTE: Do something about characters colliding into each other.
-
-let isColliding = (a: DisplayObject, b: DisplayObject): boolean => {
-    let ab: Rectangle = a.getBounds();
-    let bb: Rectangle = b.getBounds();
-    return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
-};
-
 // END GAME CONDITIONS
 
 let isOutOfBounds = (unit: Sprite): boolean => {
@@ -220,59 +213,37 @@ let handleWin = (gameMessage: PIXI.Text, message: PIXI.Text): void => {
     hasWon = true;
 };
 
+// GENERAL RESET FUNCTIONS
+
+let leftResetLeft = (unit: Sprite): void => {
+    unit.x = 62;
+};
+let leftResetRight = (unit: Sprite): void => {
+    unit.x = 718;
+};
+let rightResetLeft = (unit: Sprite): void => {
+    unit.x = 135;
+};
+let rightResetRight = (unit: Sprite): void => {
+    unit.x = 788;
+};
+
 // CYRUS RESET FUNCTIONS
 
-let leftResetCyrusY = (): void => {
+let resetCyrusY = (): void => {
     cyrus.y = 205;
 };
-let leftResetCyrusLowY = (): void => {
+let resetCyrusLowY = (): void => {
     cyrus.y = 295;
-};
-let leftResetCyrusLeft = (): void => {
-    cyrus.x = 62;
-};
-let leftResetCyrusRight = (): void => {
-    cyrus.x = 718;
 };
 
-let rightResetCyrusY = (): void => {
-    cyrus.y = 205;
-};
-let rightResetCyrusLowY = (): void => {
-    cyrus.y = 295;
-};
-let rightResetCyrusLeft = (): void => {
-    cyrus.x = 135;
-};
-let rightResetCyrusRight = (): void => {
-    cyrus.x = 788;
-};
 // HANNIT RESET FUNCTIONS
 
-let leftResetHannitY = (): void => {
+let resetHannitY = (): void => {
     hannit.y = 210;
 };
-let leftResetHannitLowY = (): void => {
+let resetHannitLowY = (): void => {
     hannit.y = 295;
-};
-let leftResetHannitLeft = (): void => {
-    hannit.x = 62;
-};
-let leftResetHannitRight = (): void => {
-    hannit.x = 718;
-};
-
-let rightResetHannitY = (): void => {
-    hannit.y = 210;
-};
-let rightResetHannitLowY = (): void => {
-    hannit.y = 295;
-};
-let rightResetHannitLeft = (): void => {
-    hannit.x = 135;
-};
-let rightResetHannitRight = (): void => {
-    hannit.x = 788;
 };
 
 // RUN GAME
@@ -280,7 +251,20 @@ let rightResetHannitRight = (): void => {
 app.ticker.add((delta: number): void => {
     for (let i: number = 0; i < enemies.length; i++) {
         cyrus.x += (A + D) * speed;
-        cyrus.y += (W + S) * speed;
+        // cyrus.y += (S) * speed;
+        if (cyrusV < 1) {
+            cyrusV = cyrusV + acc;
+        } else if ((cyrus.y >= 205 && cyrus.y <= 207 && (cyrus.x < 718 && cyrus.x > 62))) {
+            cyrusV = 0;
+            resetCyrusY();
+        } else if ((cyrus.y >= 293 && cyrus.y <= 295) && (cyrus.x < 718 && cyrus.x > 62)) {
+            cyrusV = 0;
+            resetCyrusLowY();
+        } else {
+            cyrusV = 1;
+        }
+        cyrus.y = cyrus.y + cyrusV;
+
 
         hannit.x += (left + right) * speed;
         hannit.y += (up + down) * speed;
@@ -309,16 +293,16 @@ app.ticker.add((delta: number): void => {
                 cyrus.y += .5;
             }
             if (cyrus.y >= 205 && cyrus.y <= 207 && (cyrus.x < 718 && cyrus.x > 62)) {
-                leftResetCyrusY();
+                resetCyrusY();
             }
             if ((cyrus.y >= 293 && cyrus.y <= 295) && (cyrus.x < 718 && cyrus.x > 62)) {
-                leftResetCyrusLowY();
+                resetCyrusLowY();
             }
             if ((cyrus.y <= 292 && cyrus.y > 207) && (cyrus.x > 62 && cyrus.x <= 64)) {
-                leftResetCyrusLeft();
+                leftResetLeft(cyrus);
             }
             if ((cyrus.y <= 292 && cyrus.y > 207) && (cyrus.x < 718 && cyrus.x >= 716)) {
-                leftResetCyrusRight();
+                leftResetRight(cyrus);
             }
         } else {
             if (cyrus.x >= 788 || cyrus.x <= 135) {
@@ -331,16 +315,16 @@ app.ticker.add((delta: number): void => {
                 cyrus.y += .5;
             }
             if (cyrus.y >= 205 && cyrus.y <= 207 && (cyrus.x < 788 && cyrus.x > 135)) {
-                rightResetCyrusY();
+                resetCyrusY();
             }
             if ((cyrus.y >= 293 && cyrus.y <= 295) && (cyrus.x < 788 && cyrus.x > 135)) {
-                rightResetCyrusLowY();
+                resetCyrusLowY();
             }
             if ((cyrus.y <= 292 && cyrus.y > 207) && (cyrus.x > 135 && cyrus.x <= 137)) {
-                rightResetCyrusLeft();
+                rightResetLeft(cyrus);
             }
             if ((cyrus.y <= 292 && cyrus.y > 207) && (cyrus.x < 788 && cyrus.x >= 786)) {
-                rightResetCyrusRight();
+                rightResetRight(cyrus);
             }
         }
 
@@ -357,16 +341,16 @@ app.ticker.add((delta: number): void => {
                 hannit.y += .5;
             }
             if (hannit.y >= 210 && hannit.y <= 212 && (hannit.x < 718 && hannit.x > 62)) {
-                leftResetHannitY();
+                resetHannitY();
             }
             if ((hannit.y >= 293 && hannit.y <= 295) && (hannit.x < 718 && hannit.x > 62)) {
-                leftResetHannitLowY();
+                resetHannitLowY();
             }
             if ((hannit.y <= 292 && hannit.y >= 207) && (hannit.x > 62 && hannit.x <= 64)) {
-                leftResetHannitLeft();
+                leftResetLeft(hannit);
             }
             if ((hannit.y <= 292 && hannit.y >= 207) && (hannit.x < 718 && hannit.x >= 716)) {
-                leftResetHannitRight();
+                leftResetRight(hannit);
             }
         } else {
             if (hannit.x >= 788 || hannit.x <= 135) {
@@ -379,16 +363,16 @@ app.ticker.add((delta: number): void => {
                 hannit.y += .5;
             }
             if (hannit.y >= 210 && hannit.y <= 212 && (hannit.x < 788 && hannit.x > 135)) {
-                rightResetHannitY();
+                resetHannitY();
             }
             if ((hannit.y >= 293 && hannit.y <= 295) && (hannit.x < 788 && hannit.x > 135)) {
-                rightResetHannitLowY();
+                resetHannitLowY();
             }
             if ((hannit.y <= 292 && hannit.y >= 207) && (hannit.x > 135 && hannit.x <= 137)) {
-                rightResetHannitLeft();
+                rightResetLeft(hannit);
             }
             if ((hannit.y <= 292 && hannit.y >= 207) && (hannit.x < 788 && hannit.x >= 786)) {
-                rightResetHannitRight();
+                rightResetRight(hannit);
             }
         }
     }   
