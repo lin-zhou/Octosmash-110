45c657644dd0f82bcb2b04a2325b0741520e8702
Lindsay Zhou <43657142+lin-zhou@users.noreply.github.com>
2018-11-19 22:26:59 -0500

    Create fonts-script.ts
    PIXI.Text styles

---
+++ fonts-script.ts
@@ -0,0 +1,47 @@
+export let startStyle = new PIXI.TextStyle({
+    fontFamily: "Arial",
+    fontSize: 80,
+    fontWeight: "bold",
+    fill: ["#000000", "#DE8526"], // gradient
+    stroke: "#ffffff",
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: "#000000",
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440
+});
+
+export let style = new PIXI.TextStyle({
+    fontFamily: "Arial",
+    fontSize: 36,
+    fontWeight: "bold",
+    fill: ["#ffffff", "#00ff99"], // gradient
+    stroke: "#4a1850",
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: "#000000",
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440
+});
+
+export let gameStyle = new PIXI.TextStyle({
+    fontFamily: "Arial",
+    fontSize: 75,
+    fontWeight: "bold",
+    fill: ["#ffffff", "#00ff99"], // gradient
+    stroke: "#4a1850",
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: "#000000",
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440
+});
