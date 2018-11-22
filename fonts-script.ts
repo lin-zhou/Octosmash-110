b9c0cf392d2a2339c75cb8af1f89768e95b605eb
Lindsay Zhou <43657142+lin-zhou@users.noreply.github.com>
2018-11-22 00:37:46 -0500

    Update fonts-script.ts
    Added font for character selection screen

--- fonts-script.ts
+++ fonts-script.ts
@@ -45,3 +45,19 @@ export let gameStyle = new PIXI.TextStyle({
     wordWrap: true,
     wordWrapWidth: 440
 });
+
+export let selectStyle = new PIXI.TextStyle({
+    fontFamily: "Times New Roman",
+    fontSize: 24,
+    fontWeight: "bold",
+    fill: ["#ffffff"],
+    stroke: "#455028",
+    strokeThickness: 3,
+    dropShadow: true,
+    dropShadowColor: "#000000",
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 500
+});
