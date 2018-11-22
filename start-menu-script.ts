2bc000d69a3fca47cc66eda397e69feb5082c588
Lindsay Zhou <43657142+lin-zhou@users.noreply.github.com>
2018-11-22 00:40:56 -0500

    Update start-menu-script.ts

--- start-menu-script.ts
+++ start-menu-script.ts
@@ -1,8 +1,11 @@
 import {
     Application,
     Sprite,
-    Graphics
 } from "pixi.js";
+
+import {
+    startStyle
+} from "./fonts-script";
 
 const app: Application = new Application(1024 * .85, 576 * .85);
 document.body.appendChild(app.view);
@@ -11,22 +14,6 @@ let background: Sprite = Sprite.fromImage("./Start_Screen.png");
 background.scale.x = 34 / 75;
 background.scale.y = 34 / 75;
 app.stage.addChild(background);
-
-let startStyle = new PIXI.TextStyle({
-    fontFamily: "Arial",
-    fontSize: 80,
-    fontWeight: "bold",
-    fill: ["#000000", "#DE8526"], // gradient
-    stroke: "#ffffff",
-    strokeThickness: 5,
-    dropShadow: true,
-    dropShadowColor: "#000000",
-    dropShadowBlur: 4,
-    dropShadowAngle: Math.PI / 6,
-    dropShadowDistance: 6,
-    wordWrap: true,
-    wordWrapWidth: 440
-});
 
 let startMessage = new PIXI.Text("START", startStyle);
 startMessage.x = 460;
