00bfb1476bbd7dca83a54e1bece63634b8751e62
Lindsay Zhou <43657142+lin-zhou@users.noreply.github.com>
2018-11-22 12:22:00 -0500

    Update characters-script.ts
    Created one of each character for player one and two, in case they pick the same character

--- characters-script.ts
+++ characters-script.ts
@@ -2,34 +2,66 @@ import {
     Sprite,
 } from "pixi.js";
 
-export let cyrus: Sprite = Sprite.fromImage("./Cyrus_Sprite.png");
-cyrus.scale.x = .55;
-cyrus.scale.y = .55;
+export let cyrus1: Sprite = Sprite.fromImage("./Cyrus_Sprite.png");
+cyrus1.scale.x = .55;
+cyrus1.scale.y = .55;
 
-export let hannit: Sprite = Sprite.fromImage("./Hannit_Sprite.png");
-hannit.scale.x = .5;
-hannit.scale.y = .5;
+export let hannit1: Sprite = Sprite.fromImage("./Hannit_Sprite.png");
+hannit1.scale.x = .5;
+hannit1.scale.y = .5;
 
-export let olberic: Sprite = Sprite.fromImage("./Olberic_Sprite.png");
-olberic.scale.x = .54;
-olberic.scale.y = .54;
+export let olberic1: Sprite = Sprite.fromImage("./Olberic_Sprite.png");
+olberic1.scale.x = .54;
+olberic1.scale.y = .54;
 
-export let primrose: Sprite = Sprite.fromImage("./Primrose_Sprite.png");
-primrose.scale.x = .54;
-primrose.scale.y = .54;
+export let primrose1: Sprite = Sprite.fromImage("./Primrose_Sprite.png");
+primrose1.scale.x = .54;
+primrose1.scale.y = .54;
 
-export let tressa: Sprite = Sprite.fromImage("./Tressa_Sprite.png");
-tressa.scale.x = .54;
-tressa.scale.y = .54;
+export let tressa1: Sprite = Sprite.fromImage("./Tressa_Sprite.png");
+tressa1.scale.x = .54;
+tressa1.scale.y = .54;
 
-export let ophelia: Sprite = Sprite.fromImage("./Ophelia_Sprite.png");
-ophelia.scale.x = .53;
-ophelia.scale.y = .53;
+export let ophelia1: Sprite = Sprite.fromImage("./Ophelia_Sprite.png");
+ophelia1.scale.x = .53;
+ophelia1.scale.y = .53;
 
-export let therion: Sprite = Sprite.fromImage("./Therion_Sprite.png");
-therion.scale.x = .58;
-therion.scale.y = .58;
+export let therion1: Sprite = Sprite.fromImage("./Therion_Sprite.png");
+therion1.scale.x = .58;
+therion1.scale.y = .58;
 
-export let alfyn: Sprite = Sprite.fromImage("./Alfyn_Sprite.png");
-alfyn.scale.x = .55;
-alfyn.scale.y = .55;
+export let alfyn1: Sprite = Sprite.fromImage("./Alfyn_Sprite.png");
+alfyn1.scale.x = .55;
+alfyn1.scale.y = .55;
+
+export let cyrus2: Sprite = Sprite.fromImage("./Cyrus_Sprite.png");
+cyrus2.scale.x = .55;
+cyrus2.scale.y = .55;
+
+export let hannit2: Sprite = Sprite.fromImage("./Hannit_Sprite.png");
+hannit2.scale.x = .5;
+hannit2.scale.y = .5;
+
+export let olberic2: Sprite = Sprite.fromImage("./Olberic_Sprite.png");
+olberic2.scale.x = .54;
+olberic2.scale.y = .54;
+
+export let primrose2: Sprite = Sprite.fromImage("./Primrose_Sprite.png");
+primrose2.scale.x = .54;
+primrose2.scale.y = .54;
+
+export let tressa2: Sprite = Sprite.fromImage("./Tressa_Sprite.png");
+tressa2.scale.x = .54;
+tressa2.scale.y = .54;
+
+export let ophelia2: Sprite = Sprite.fromImage("./Ophelia_Sprite.png");
+ophelia2.scale.x = .53;
+ophelia2.scale.y = .53;
+
+export let therion2: Sprite = Sprite.fromImage("./Therion_Sprite.png");
+therion2.scale.x = .58;
+therion2.scale.y = .58;
+
+export let alfyn2: Sprite = Sprite.fromImage("./Alfyn_Sprite.png");
+alfyn2.scale.x = .55;
+alfyn2.scale.y = .55;
