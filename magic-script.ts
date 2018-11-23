9c06199f11792912a5f545914b22730d175a0f3b
Lindsay Zhou <43657142+lin-zhou@users.noreply.github.com>
2018-11-22 23:14:28 -0500

    Update magic-script.ts

--- magic-script.ts
+++ magic-script.ts
@@ -8,22 +8,52 @@ document.body.appendChild(app.view);
 
 class Magic {
     sprite: Sprite;
+    x: number = 0;
+    y: number = 0;
     direction: number = 1;
+    vel: number = 0;
     constructor(sprite: Sprite) {
         this.sprite = sprite;
     }
+    getPoint(unitX: number, unitY: number): void {
+        this.sprite.x += unitX;
+        this.sprite.y += unitY;
+    }
 }
 
-let shootCount1 = 0;
-let magic = new Magic(Sprite.fromImage("./Magic_Blast.png"));
+let magicArr: Magic[] = [];
+
+let keyThree: number = 0;
 
 window.addEventListener("keydown", (e: KeyboardEvent): void  => {
+    console.log("key: " + e.keyCode);
     const ATTACK: number = 51;
 
     if (e.keyCode === ATTACK) {
-        shootCount1++;
-        magic = new Magic(Sprite.fromImage("./Magic_Blast.png"));
-        magic.sprite.x = 50 + shootCount1 * 100;
-        app.stage.addChild(magic.sprite);
+        keyThree = 1;
+        if (keyThree === 1) {
+            let sprite: Sprite = Sprite.fromImage("./Magic_Blast.png");
+            let magic: Magic = new Magic(sprite);
+            magic.getPoint(50, 50);
+            magicArr.push(magic);
+            app.stage.addChild(magic.sprite);
+        }
     }
 },                      false);
+
+window.addEventListener("keyup", (e: KeyboardEvent): void  => {
+    console.log("key: " + e.keyCode);
+    const ATTACK: number = 51;
+
+    if (e.keyCode === ATTACK) {
+        keyThree = 0;
+    }
+    
+},                      false);
+
+app.ticker.add((delta: number): void => {
+    for (let i: number = 0; i < magicArr.length; i++) {
+        let magic: Magic = magicArr[i];
+        magic.sprite.x += 10 * magic.direction;
+    }
+},             false);
