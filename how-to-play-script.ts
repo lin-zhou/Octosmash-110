ed0550adef87d4595f2d85f175f469840db9ed5c
Lindsay Zhou <43657142+lin-zhou@users.noreply.github.com>
2018-11-24 03:29:15 -0500

    Create how-to-play-script.ts

---
+++ how-to-play-script.ts
@@ -0,0 +1,101 @@
+import { 
+    Application,
+    Sprite,
+    Graphics
+    } from "pixi.js"; 
+
+import {
+    howToTitleStyle,
+    howToStyle,
+} from "./fonts-script";
+
+const app: Application = new Application(1024 * .85, 576 * .85);
+document.body.appendChild(app.view);
+
+const htBG: Sprite = Sprite.fromImage("./how_to_background.png");
+htBG.scale.x = 1088 / 1175;
+htBG.scale.y = 2448 / 2645;
+app.stage.addChild(htBG);
+
+const black: Graphics = new Graphics();
+black.beginFill(0x000000, .7);
+black.drawRect(0, 0, 1024 * .85, 576 * .85);
+app.stage.addChild(black);
+
+const howTo = new PIXI.Text("HOW TO PLAY", howToTitleStyle);
+howTo.x = 325;
+howTo.y = 25;
+app.stage.addChild(howTo);
+
+const WASD: Sprite = Sprite.fromImage("./WASD.png");
+WASD.scale.x = .1;
+WASD.scale.y = .1;
+WASD.x = 22;
+WASD.y = 90;
+app.stage.addChild(WASD);
+
+const p1Move = new PIXI.Text("Player One moves with WASD.", howToStyle);
+p1Move.x = 110;
+p1Move.y = 95;
+app.stage.addChild(p1Move);
+
+const arrowKeys: Sprite = Sprite.fromImage("./Arrow_Keys.png");
+arrowKeys.scale.x = .3;
+arrowKeys.scale.y = .3;
+arrowKeys.x = 450;
+arrowKeys.y = 92;
+app.stage.addChild(arrowKeys);
+
+const p2Move = new PIXI.Text("Player Two moves with arrow keys.", howToStyle);
+p2Move.x = 525;
+p2Move.y = 95;
+app.stage.addChild(p2Move);
+
+const threeKey: Sprite = Sprite.fromImage("./3_Key.png");
+threeKey.x = 37;
+threeKey.y = 180;
+app.stage.addChild(threeKey);
+
+const p1Attack = new PIXI.Text("Player One attacks using the 3 key.", howToStyle);
+p1Attack.x = 79;
+p1Attack.y = 175;
+app.stage.addChild(p1Attack);
+
+const backslash: Sprite = Sprite.fromImage("./Backslash_Key.png");
+backslash.scale.x = .2;
+backslash.scale.y = .2;
+backslash.x = 477;
+backslash.y = 177;
+app.stage.addChild(backslash);
+
+const p2Attack = new PIXI.Text("Player Two attacks with the backslash.", howToStyle);
+p2Attack.x = 525;
+p2Attack.y = 185;
+app.stage.addChild(p2Attack);
+
+const explanation0 = new PIXI.Text("Each player can only have four attacks on screen at once.", howToStyle);
+explanation0.x = 200;
+explanation0.y = 260;
+app.stage.addChild(explanation0);
+
+const explanation1 = new PIXI.Text("Knock your opponent off the screen to win.", howToStyle);
+explanation1.x = 252;
+explanation1.y = 288;
+app.stage.addChild(explanation1);
+
+const pressToStart = new PIXI.Text("PRESS                                                   TO PLAY", howToTitleStyle);
+pressToStart.x = 120;
+pressToStart.y = 360;
+app.stage.addChild(pressToStart);
+
+const spacebar: Sprite = Sprite.fromImage("./Space_Bar.png");
+spacebar.scale.x = .9;
+spacebar.scale.y = .9;
+spacebar.x = 221;
+spacebar.y = 343;
+app.stage.addChild(spacebar);
+
+window.addEventListener("keydown", (e: KeyboardEvent): void  => {
+    console.log("To selection...");
+    // Character Selection starts here!
+},                      false);
